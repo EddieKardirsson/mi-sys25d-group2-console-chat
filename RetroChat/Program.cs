a792ff23dbf7d0837c5e3e93cdd33e3e838b18a3
Frida <lalunaaa00@gmail.com>
2025-11-05 16:32:23 +0100

    Username input

--- RetroChat/Program.cs
+++ RetroChat/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your username: ");
+        string? input = Console.ReadLine();
+
+        while (input == null)
+        {
+            Console.WriteLine("Invalid input");
+            Console.WriteLine("Enter your username: ");
+            input = Console.ReadLine(); 
+        }
+
+        Console.WriteLine($"{input} is logged in");
     }
 }

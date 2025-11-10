a86bbb29fc04eb115a4d97d4c93b6d0547ed0c22
Frida <lalunaaa00@gmail.com>
2025-11-10 18:48:03 +0100

    Adds messages handling
    Introduces classes for message creation, sending, and receiving. Incorporates timestamping and user association
    for each message. Implements message serialization for network transmission.

---
+++ RetroChat/messages.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+namespace RetroChat;
+
+public class Messages
+{
+    public string Text { get; set; }
+    public User User { get; set; }
+    public DateTime TimeStamp { get; set; }
+
+    public Message(string text, User user)
+    {
+        Text = text;
+        User = user;
+        TimeStamp = DateTime.Now;
+    }
+
+    public async Task SendMessage(User user, string inMessage, string eventName)
+    {
+        string jsonMessage = JsonSerializer.Serialize(this);
+        string outMessage = $"\\n{user.Name} [{TimeStamp}]: \\{inMessage}";
+
+        await SocketManager.Client.EmitAsync(eventName, jsonMessage);
+        Console.WriteLine(outMessage);
+    }
+
+    public static async Task ReceiveMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}

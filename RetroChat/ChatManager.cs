167f45a76adb40ca9f815ffd5caeaa4ec53b66da
Edison Kardirsson <ed.kardirsson@hotmail.com>
2025-11-10 19:33:28 +0100

    feat: Implement ChatManager and cache users

---
+++ RetroChat/ChatManager.cs
@@ -0,0 +1,88 @@
+using System.Text.Json;
+
+namespace RetroChat;
+
+public class ChatManager
+{
+    private static List<User> _storedUsers = new List<User>();
+    public static User? LoggedInUser { get; private set; }
+    
+    private static readonly string DataFilePath = "./localData/";
+    private static readonly string UserFilePath = DataFilePath + "users.json";
+    
+    #region StartUp
+    
+    public User StartUp()
+    {
+        InitializeUserDirectoryAndLoadCache();
+
+        string username = PromptForUsername();
+        
+        User user = _storedUsers.Any(u => u.Name == username) ? LoadExistingUser(username) : CreateNewUser(username);
+        
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey(false);
+        return user;
+    }
+
+    #region Private Methods for StartUp
+    
+    private static void InitializeUserDirectoryAndLoadCache()
+    {
+        if(!Directory.Exists(UserFilePath)) Directory.CreateDirectory(DataFilePath);
+
+        if (File.Exists(UserFilePath))
+        {
+            string json = File.ReadAllText(UserFilePath);
+            _storedUsers = JsonSerializer.Deserialize<List<User>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+            Console.WriteLine($"Loaded {_storedUsers.Count} users from cache");
+        }
+        else Console.WriteLine("No users found in cache");
+    }
+
+    private static string PromptForUsername()
+    {
+        Console.WriteLine("Enter your username: ");
+        string? username = Console.ReadLine();
+
+        while (username == null)
+        {
+            Console.WriteLine("Invalid input! Username cannot be empty");
+            Console.WriteLine("Please, enter your username: ");
+            username = Console.ReadLine(); 
+        }
+        Console.WriteLine($"{username} is logged in");
+        
+        return username;
+    }
+
+    private User LoadExistingUser(string username)
+    {
+        User user = _storedUsers.First(u => u.Name == username);
+        LoggedInUser = user;
+        Console.WriteLine("User already exists. Loading user...");
+        return user;
+    }
+
+    private User CreateNewUser(string username)
+    {
+        Console.WriteLine("User does not exist. Creating new user...");
+        
+        User user = new User(username);
+        LoggedInUser = user;
+        
+        string usersJson = JsonSerializer.Serialize(_storedUsers, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+        File.WriteAllText(UserFilePath, usersJson);
+        return user;
+    }
+    
+    #endregion
+    #endregion
+}
